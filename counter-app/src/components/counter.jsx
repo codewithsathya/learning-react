ea820146d12ab875bfde906bd660a15adda273e9
S Sathiya <codewithsathya@gmail.com>
2021-05-27 15:49:19 +0530

    Passing values to components

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
         <button
           onClick={() => this.handleIncrement()}
           className="btn btn-secondary btn-sm"
@@ -25,13 +25,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+  formatValue() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -5,7 +5,7 @@ class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 0 },
-      { id: 2, value: 0 },
+      { id: 2, value: 3 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
       { id: 5, value: 0 },
@@ -15,7 +15,7 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} />
         ))}
       </div>
     );
