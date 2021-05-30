eb453826547c7c788149ed12684fdbe12009397f
S Sathiya <codewithsathya@gmail.com>
2021-05-30 16:49:03 +0530

    Filtering - displaying items

--- vidly/src/components/common/listGroup.jsx
+++ vidly/src/components/common/listGroup.jsx
@@ -1,8 +1,16 @@
-import React, { Component } from "react";
-import { getGenres } from "../../services/fakeGenreService";
+import React from "react";
 
-const ListGroup = () => {
-  return null;
+const ListGroup = (props) => {
+  const { items, textProperty, valueProperty } = props;
+  return (
+    <ul className="list-group">
+      {items.map((genre) => (
+        <li key={genre[valueProperty]} className="list-group-item">
+          {genre[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ListGroup;
