0947c84b993571e26b59879b7b951332108fe88d
S Sathiya <codewithsathya@gmail.com>
2021-06-01 15:57:25 +0530

    Sorting - Extracting table

--- vidly/src/components/common/tableHeader.jsx
+++ vidly/src/components/common/tableHeader.jsx
@@ -32,7 +32,7 @@ class TableHeader extends Component {
             <th
               className="clickable"
               key={column.path || column.key}
-              onClick={() => this.raiseSort(column.path)}
+              onClick={(column.path) ? () => this.raiseSort(column.path) : void(0)}
             >
               {column.label} {this.getSortIcon(column)}
             </th>
