d2f7cb402bda71ab10099801aac417aac760c92b
S Sathiya <codewithsathya@gmail.com>
2021-06-01 15:13:52 +0530

    Sorting - Rendering cell content

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import TableHeader from "./common/tableHeader";
-import TableBody from "./common/tableBody"
+import TableBody from "./common/tableBody";
 import Like from "./common/like";
 
 class MoviesTable extends Component {
@@ -9,8 +9,23 @@ class MoviesTable extends Component {
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
-    {key: "like"},
-    {key: "delete"},
+    {
+      key: "like",
+      content: (movie) => (
+        <Like liked={movie.liked} onLike={() => this.props.onLike(movie)} />
+      ),
+    },
+    {
+      key: "delete",
+      content: (movie) => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      ),
+    },
   ];
 
   raiseSort = (path) => {
@@ -25,32 +40,15 @@ class MoviesTable extends Component {
   };
 
   render() {
-    const { movies, onDelete, onLike, onSort, sortColumn } = this.props;
+    const { movies, onSort, sortColumn } = this.props;
     return (
       <table className="table">
-        <TableHeader columns={this.columns} sortColumn={sortColumn} onSort={onSort}/>
-        <TableBody data={movies} columns={this.columns}/>
-        <tbody>
-          {movies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like liked={movie.liked} onLike={() => onLike(movie)} />
-              </td>
-              <td>
-                <button
-                  onClick={() => onDelete(movie)}
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <TableHeader
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
+        <TableBody data={movies} columns={this.columns} />
       </table>
     );
   }
