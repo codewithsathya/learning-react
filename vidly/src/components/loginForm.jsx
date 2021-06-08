c53b43e565fa07f103386e3d21942ce14a15c2cf
S Sathiya <codewithsathya@gmail.com>
2021-06-08 19:06:59 +0530

    Using Refs

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  username = React.createRef();
+
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("Submitted");
   };
+
+  componentDidMount() {
+    this.username.current.focus();
+  };
+
   render() {
     return (
       <div className="container">
@@ -12,7 +19,7 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input ref={this.username} id="username"  type="text" className="form-control" />
           </div>
           <div className="form-group">
             <label htmlFor="">Password</label>
