df794b32f84ea1789338d86ca8520c3ac1ffa2cf
S Sathiya <codewithsathya@gmail.com>
2021-06-09 18:36:14 +0530

    Validation on change

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -7,30 +7,43 @@ class LoginForm extends Component {
     errors: {},
   };
 
-  validate = () => {
-    const errors = {};
-    const { account } = this.state;
-    if (account.username.trim() === "") {
-      errors.username = "Username is required.";
+  // validate = () => {
+  //   const errors = {};
+  //   const { account } = this.state;
+  //   if (account.username.trim() === "") {
+  //     errors.username = "Username is required.";
+  //   }
+  //   if (account.password.trim() === "") {
+  //     errors.password = "Password is required.";
+  //   }
+  //   return Object.keys(errors).length === 0 ? null : errors;
+  // };
+
+  validateProperty = ({name, value}) => {
+    if(name === 'username') {
+      if(value.trim() === '') return "Username is required.";
     }
-    if (account.password.trim() === "") {
-      errors.password = "Password is required.";
+    if(name === 'password') {
+      if(value.trim() === '') return "Password is required.";
     }
-    return Object.keys(errors).length === 0 ? null : errors;
-  };
+  }
 
   handleChange = ({ currentTarget: input }) => {
+    const errors = {...this.state.errors};
+    const errorMessage = this.validateProperty(input);
+    if(errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
 
-    const errors = this.validate();
-    this.setState({ errors: errors || {} }); //Returns empty object instead of null
-    if (errors) return;
+    // const errors = this.validate();
+    // this.setState({ errors: errors || {} }); //Returns empty object instead of null
+    // if (errors) return;
 
     //Server
     console.log("Submitted");
