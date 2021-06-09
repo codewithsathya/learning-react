b95e2caf556c95943498577e373c2e232149ca7c
S Sathiya <codewithsathya@gmail.com>
2021-06-09 21:59:21 +0530

    Validating a field using Joi

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -25,9 +25,9 @@ class LoginForm extends Component {
 
   validateProperty = ({ name, value }) => {
     const obj = { [name]: value };
-    const schema = { [name]: this.schema.username };
+    const schema = { [name]: this.schema[name] };
     const { error } = Joi.validate(obj, schema);
-    return error ? error.details.message : null;
+    return error ? error.details[0].message : null;
   };
 
   handleChange = ({ currentTarget: input }) => {
