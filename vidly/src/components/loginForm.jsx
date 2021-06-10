5a1b97c8b4aef10780ab0088fe5eea735ce95052
S Sathiya <codewithsathya@gmail.com>
2021-06-10 10:32:54 +0530

    Seperate register form

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import Joi from "joi-browser";
-import Input from "./common/input";
 import Form from "./common/form";
 
 class LoginForm extends Form {
   state = {
-    data: { email: "", password: "", name: "" },
+    data: { username: "", password: "" },
     errors: {},
   };
 
   schema = {
-    email: Joi.string().email().required().label("Email"),
-    password: Joi.string().required().label("Password").min(5),
-    name: Joi.string().required().label("Name"),
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
   doSubmit = () => {
@@ -25,9 +23,8 @@ class LoginForm extends Form {
       <div className="container">
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput("email", "Email")}
+          {this.renderInput("username", "Username")}
           {this.renderInput("password", "Password", "password")}
-          {this.renderInput("name", "Name")}
           {this.renderButton("Login")}
         </form>
       </div>
