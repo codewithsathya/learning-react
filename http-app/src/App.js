711fe32938e8992c83e4cd7bc61af1a741649b62
S Sathiya <codewithsathya@gmail.com>
2021-06-11 09:51:27 +0530

    Creating data

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -2,20 +2,22 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
 
+const apiEndPoint = "https://jsonplaceholder.typicode.com/posts";
 class App extends Component {
   state = {
     posts: [],
   };
 
   async componentDidMount() {
-    const { data: posts } = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
+    const { data: posts } = await axios.get(apiEndPoint);
     this.setState({ posts });
   }
 
-  handleAdd = () => {
-    console.log("Add");
+  handleAdd = async () => {
+    const obj = { title: "a", body: "b" };
+    const { data: post } = await axios.post(apiEndPoint, obj);
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
   };
 
   handleUpdate = (post) => {
