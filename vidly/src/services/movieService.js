5739816e0697db801b55524a5d324c0da72d072c
S Sathiya <codewithsathya@gmail.com>
2021-06-12 17:15:32 +0530

    Extracting a config file

--- vidly/src/services/movieService.js
+++ vidly/src/services/movieService.js
@@ -1,11 +1,12 @@
 import http from "./httpService";
-import config from "../config.json"
-import { saveMovie } from './fakeMovieService';
+import { apiUrl } from "../config.json";
+
+const apiEndPoint = apiUrl + "/movies";
 
 export function getMovies() {
-	return http.get(config.moviesEndPoint);
+  return http.get(apiEndPoint);
 }
 
 export function deleteMovie(movieId) {
-	return http.delete(config.moviesEndPoint + "/" + movieId);
+  return http.delete(apiEndPoint + "/" + movieId);
 }
