5796ea919038ddced256845e839e2184d0e6a736
S Sathiya <codewithsathya@gmail.com>
2021-06-15 18:02:50 +0530

    Submitting the login form

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { login } from "./../services/authService";
 
 class LoginForm extends Form {
   state = {
@@ -13,9 +14,9 @@ class LoginForm extends Form {
     password: Joi.string().required().label("Password"),
   };
 
-  doSubmit = () => {
-    //Server
-    console.log("Submitted");
+  doSubmit = async () => {
+    const { username, password } = this.state.data;
+    await login(username, password);
   };
 
   render() {
