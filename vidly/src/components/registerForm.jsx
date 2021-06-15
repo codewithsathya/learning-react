82a790608d75812f021a3dcdec659a2d3435fdb4
S Sathiya <codewithsathya@gmail.com>
2021-06-15 17:40:22 +0530

    Handling registration errrors

--- vidly/src/components/registerForm.jsx
+++ vidly/src/components/registerForm.jsx
@@ -16,7 +16,15 @@ class RegisterForm extends Form {
   };
 
   doSubmit = async () => {
-    await UserService.register(this.state.data);
+    try {
+      await UserService.register(this.state.data);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.email = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
